0d1bcbe7d79d5cf3dd57486222d9a516efba7b00
chenchungwan <142457798+chenchungwan@users.noreply.github.com>
2025-08-28 15:10:03 -0700

    after first lesson

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        LazyVGrid(columns: columns, spacing: 5) {
+            CardView(isFaceUp: false, character: "🤣")
+            CardView(isFaceUp: true, character: "🤣")
+            CardView(isFaceUp: false, character: "🤣")
+            CardView(isFaceUp: true, character: "🤣")
         }
         .padding()
     }
 }
+
+
+
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var character: String
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 10).strokeBorder(lineWidth: 1)
+                Text(character).font(.largeTitle).padding()
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+            }
+        }
+        .foregroundColor(.blue)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
